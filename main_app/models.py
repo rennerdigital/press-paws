150e754c2afdff03e87c88c20e16b22e868caf67
Renner <rennermccreath@gmail.com>
2021-06-26 13:15:04 -0400

    added Hotel model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,18 @@
 from django.db import models
-from 
+from django.contrib.auth.models import User
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField(max_length=10)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+
 # Create your models here.
 # test:sam
 # test:renner
