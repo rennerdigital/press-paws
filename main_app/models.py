f80ef43abaabc0abfac494cd5f34161da7fddae1
Renner <rennermccreath@gmail.com>
2021-06-26 13:30:09 -0400

    updated hoteh room and profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Hotel(models.Model):
@@ -11,6 +12,29 @@ class Hotel(models.Model):
     def __str__(self):
         return self.name
 
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField(max_length=2)
+    price = models.FloatField("Price Per Night ($CAD)")
+    description = models.TextField(max_length=250)
+    is_dog_friendly = models.BooleanField()
+    is_cat_friendly = models.BooleanField()
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'room_id': self.id})
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.IntegerField(max_length=10)
+    address = models.CharField(max_length=100)
+    credit_card = models.IntegerField(max_length=16)
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
