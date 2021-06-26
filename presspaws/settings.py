49cebf4e6d7f6638910281c440dca8162006641c
Anna Baranova <asbaranova1@gmail.com>
2021-06-26 17:21:57 -0400

    add room detail template

--- presspaws/settings.py
+++ presspaws/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'presspaws',
-        # "HOST": 'localhost',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '',
+        "HOST": 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
