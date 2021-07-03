3fa9a342f7a85c60a17ed5497de0cbfec76b3c20
Anna Baranova <asbaranova1@gmail.com>
2021-07-02 21:24:21 -0400

    add save method to model reservation

--- main_app/views.py
+++ main_app/views.py
@@ -147,8 +147,6 @@ def create_reservation(request):
         if new_reservation.at_least_one_night() == False:
           days_error_message = "You have to stay longer!"
         else:
-          new_reservation.calculate_nights()
-          new_reservation.calculate_price()
           new_reservation.save()
           return redirect ('reservation_index')
     else:
@@ -208,8 +206,6 @@ def room_create_reservation(request, room_id):
         if new_reservation.at_least_one_night() == False:
           days_error_message = "You have to stay longer!"
         else:
-          new_reservation.calculate_nights()
-          new_reservation.calculate_price()
           new_reservation.save()
           return redirect ('reservation_index')
 
