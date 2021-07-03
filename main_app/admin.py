a996c70bd0da07ce5b433aeb75d0f4629c3f6317
Anna Baranova <asbaranova1@gmail.com>
2021-07-02 22:59:58 -0400

    add feedback model and form

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Hotel, Room, Profile, Reservation, Pet, Photo
+from .models import Hotel, Room, Profile, Reservation, Pet, Photo, Feedback
 
 # Register your models here.
 admin.site.register(Hotel)
@@ -8,3 +8,4 @@ admin.site.register(Profile)
 admin.site.register(Reservation)
 admin.site.register(Pet)
 admin.site.register(Photo)
+admin.site.register(Feedback)
